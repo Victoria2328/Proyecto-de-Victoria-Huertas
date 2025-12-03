69d806310fc67ae904fb3d1d0c8cce435fec227f
Victoria2328 <vhuertasortiz@gmail.com>
2025-12-03 16:28:02 -0500

    Fix SonarQube error by updating comment in conexion.php
    Removed a comment that was causing an error in SonarQube.

--- venta_carros/conexion.php
+++ venta_carros/conexion.php
@@ -5,6 +5,7 @@ $contraseña = "";
 $database = "venta_carros";
 
 $conn = new mysqli($servername, $username, $contraseña, $database);
+// (Este es solo un comentario y ya no causa error en SonarQube)//
 
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
